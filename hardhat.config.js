21993c3efdb19c3d1486e717e1ed516071eff1f1
elixirevo <elixirevocation@gmail.com>
2022-05-24 02:46:19 +0900

    update: network

--- hardhat.config.js
+++ hardhat.config.js
@@ -9,6 +9,10 @@ module.exports = {
       url: 'https://dataseed.popcateum.org',
       accounts: [process.env.PK || ''],
     },
+    gorli: {
+      url: 'https://goerli.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161',
+      accounts: [process.env.TestPK || ''],
+    },
   },
   abiExporter: {
     path: './abi',
